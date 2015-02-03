b41c59bf539a5979b8c23bcfd479deda45baf59c
Kristoffer Andergrim <andergrim@gmail.com>
2015-02-03 20:56:47 +0100

    Converter libreoffice control script

--- scripts/libreoffice.sh
+++ scripts/libreoffice.sh
@@ -2,16 +2,14 @@
 #
 # Script for starting/stopping LibreOffice without restarting Alfresco
 # 
-# Copyright 2013 Loftux AB, Peter Löfgren
+# Copyright 2015, Kristoffer Andergrim
+# Based on alfresco-ubuntu-install by Peter Löfgren, Loftux AB
 # Distributed under the Creative Commons Attribution-ShareAlike 3.0 Unported License (CC BY-SA 3.0)
 # -------
 
-## TODO - run libreoffice/soffice as a daemon
-## http://osdir.com/ml/freebsd-questions/2012-03/msg00629.html
-
     # User under which tomcat will run
-    USER=alfresco
-    ALF_HOME=/opt/alfresco
+    USER="alfresco"
+    ALF_HOME="/opt/alfresco"
     cd "$ALF_HOME"
     # export LC_ALL else openoffice may use en settings on dates etc
     export LC_ALL=@@LOCALESUPPORT@@
@@ -20,39 +18,34 @@
     RETVAL=0
 
     start() {
-        OFFICE_PORT=`ps ax|grep office|grep 8100|wc -l`
-        if [ $OFFICE_PORT -ne 0 ]; then
-            echo "Alfresco Open Office service already started"
-	        CURRENT_PROCID=`ps axf|grep office|grep 8100|awk -F " " 'NR==1 {print $1}'`
-	        echo $CURRENT_PROCID
+        OFFICE_PORT=`ps axww|grep soffice.bin|grep 8100|wc -l`
+        if [ "$OFFICE_PORT" -ne "0" ]; then
+            CURRENT_PROCID=`ps axfww|grep soffice.bin|grep 8100|awk -F " " 'NR==1 {print $1}'`
+            echo "Alfresco Open Office service already started: $CURRENT_PROCID"
         else
-            #Only start if Alfresco is already running
-            SHUTDOWN_PORT=`netstat -vatn|grep LISTEN|grep 8005|wc -l`
-            export JAVA_HOME=$JAVA_HOME
-            if [ $SHUTDOWN_PORT -ne 0 ]; then
-	        #/bin/su -s /bin/bash $USER -c "/usr/lib/openoffice/program/soffice.bin \"-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager\" \"-env:UserInstallation=file:///opt/alfresco/alf_data/oouser\" -nologo -headless -nofirststartwizard -norestore &" >/dev/null
-	        /bin/su -s /bin/bash $USER -c "/opt/libreoffice4.2/program/soffice.bin \"--accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager\" \"-env:UserInstallation=file:///opt/alfresco/alf_data/oouser\" --nologo --headless --nofirststartwizard --norestore --nodefault &" >/dev/null
-            echo "Alfresco Open Office starting"
-	        logger Alfresco Open Office service started
-            fi
+            # Only start if Alfresco is already running
+            SHUTDOWN_PORT=`sockstat | grep 8005 | wc -l`
 
+            if [ "$SHUTDOWN_PORT" -ne "0" ]; then
+              su -m ${USER} -c '/usr/local/lib/libreoffice/program/soffice.bin -env:UserInstallation=file:///opt/alfresco/alf_data/oouser --accept="socket,host=127.0.0.1,port=8100;urp;StarOffice.ServiceManager" --headless --nocrashreport --nofirststartwizard --nologo --norestore & >/dev/null'
+              echo "Alfresco Open Office starting"
+            fi
         fi
     }
     stop() {
-        # Start Tomcat in normal mode
-        OFFICE_PORT=`ps ax|grep office|grep 8100|wc -l`
-        if [ $OFFICE_PORT -ne 0 ]; then
-            echo "Alfresco Open Office started, killing"
-	        CURRENT_PROCID=`ps axf|grep office|grep 8100|awk -F " " 'NR==1 {print $1}'`
-	        echo $CURRENT_PROCID
+        OFFICE_PORT=`ps axww|grep soffice.bin|grep 8100|wc -l`
+        if [ "$OFFICE_PORT" -ne "0" ]; then
+            echo "Alfresco Open Office started, killing $CURRENT_PROCID"
+	        CURRENT_PROCID=`ps axfww|grep soffice.bin|grep 8100|awk -F " " 'NR==1 {print $1}'`
 	        kill $CURRENT_PROCID
-	        logger Alfresco Open Office service stopped
+        else
+            echo "Not started."
         fi
     }
     status() {
         # Start Tomcat in normal mode
-        OFFICE_PORT=`ps ax|grep office|grep 8100|wc -l`
-        if [ $OFFICE_PORT -ne 0 ]; then
+        OFFICE_PORT=`ps axww|grep soffice.bin|grep 8100|wc -l`
+        if [ "$OFFICE_PORT" -ne 0 ]; then
             echo "Alfresco LibreOffice service started"
         else
             echo "Alfresco LibreOffice service NOT started"
